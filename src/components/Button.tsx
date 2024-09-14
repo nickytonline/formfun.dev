0971a61631ebc11b9ded791a35b0c70a3829c29a
Nick Taylor <nick@nickyt.co>
2024-09-14 09:13:17 -0400

    tightened up button styling and props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,16 +1,19 @@
-import { splitProps, type JSXElement, type JSX } from "solid-js";
+import { splitProps, type JSX } from "solid-js";
 
-interface ButtonProps extends JSX.ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: "primary" | "secondary";
-  onClick?: (event: MouseEvent) => void;
-  children: JSXElement;
+interface ButtonProps
+  extends Omit<
+    JSX.ButtonHTMLAttributes<HTMLButtonElement>,
+    "className" | "class"
+  > {
+  variant: "primary" | "secondary";
 }
 
 export const Button = (props: ButtonProps) => {
   const [local, others] = splitProps(props, ["variant", "onClick", "children"]);
 
   const getButtonClass = () => {
-    const baseClass = "px-2 py-1 rounded-md w-fit transition-colors";
+    const baseClass =
+      "px-2 py-1 rounded-md w-fit transition-colors w-full sm:w-fit";
     const variantClass =
       local.variant === "secondary"
         ? "bg-gray-500 text-white hover:bg-gray-600"
